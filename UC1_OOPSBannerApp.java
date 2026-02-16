66a66752a48f3597575640f3591b070c4991cec9
Anushka-0233 <ak0233@srmist.edu.in>
2026-02-16 17:21:14 +0530

    Added separate files for UC1 and UC2 OOPS Banner App

---
+++ UC1_OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * UC1: Print OOPS to Console
+ *
+ * @author Anushka
+ * @version 1.0
+ */
+public class UC1_OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
---
+++ UC2_OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author Anushka
+ * @version 2.0
+ */
+public class UC2_OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " ******  " + "  " + "  *****  ");
+        System.out.println(" **   ** " + "  " + " **   ** " + "  " + " **   ** " + "  " + " **   ** ");
+        System.out.println(" **   ** " + "  " + " **   ** " + "  " + " **   ** " + "  " + " **       ");
+        System.out.println(" **   ** " + "  " + " **   ** " + "  " + " ******  " + "  " + "  *****  ");
+        System.out.println(" **   ** " + "  " + " **   ** " + "  " + " **      " + "  " + "       ** ");
+        System.out.println(" **   ** " + "  " + " **   ** " + "  " + " **      " + "  " + " **   ** ");
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " **      " + "  " + "  *****  ");
+    }
+}
